1fe6bd46d07ea5c2803a8e656ed00e6b3aff739d
Kornel Lugosi <coornail@gmail.com>
2018-06-02 12:12:51 +0200

    Download articles in parallel

--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"regexp"
 
@@ -20,12 +23,13 @@ const clearScreen = "\033[H\033[2J"
 
 // Flags
 var (
-	state     string
-	outputDir string
-	domain    string
-	tag       string
-	search    string
-	force     bool
+	state       string
+	outputDir   string
+	domain      string
+	tag         string
+	search      string
+	force       bool
+	parallelism int
 
 	consumerKey string
 )
@@ -39,6 +43,7 @@ func init() {
 	flag.StringVar(&search, "search", "", "Search to limit the archiving to")
 	flag.StringVar(&tag, "tag", "", "Tag to limit the archiving to")
 	flag.BoolVar(&force, "force", false, "Download already downloaded articles")
+	flag.IntVar(&parallelism, "parallelism", 8, "Number of threads to download the articles")
 	flag.Parse()
 
 	if state != string(api.StateUnread) && state != string(api.StateAll) && state != string(api.StateArchive) {
@@ -50,6 +55,10 @@ func init() {
 }
 
 func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
 	client, err := GetClient()
 	if err != nil {
 		panic(err)
@@ -63,13 +72,15 @@ func main() {
 	}
 	res, err := client.Retrieve(options)
 	if err != nil {
-		panic(err)
+		fmt.Printf("%s\n", err)
+		os.Exit(1)
 	}
 
 	// Create download directory if it doesn't exist.
 	if _, err := os.Stat(outputDir); os.IsNotExist(err) {
 		if err = os.MkdirAll(outputDir, 0744); err != nil {
-			panic(err)
+			fmt.Printf("%s\n", err)
+			os.Exit(1)
 		}
 	}
 
@@ -78,37 +89,89 @@ func main() {
 	bar.AppendCompleted()
 
 	// Append the currently processed title
-	var title string
-	var maxTitleLength int
+	var currentTitle string
 	bar.AppendFunc(func(b *uiprogress.Bar) string {
-		res := fmt.Sprintf("[%d\t/%d] %s", bar.Current(), len(res.List), title)
-		for i := 0; i < maxTitleLength-len(title); i++ {
+		renderedTitle := currentTitle
+
+		// Do not render longer lines than the width of the terminal.
+		w, _ := getWidth()
+		maxTitleLength := int(w) - b.Width - 32
+		if len(renderedTitle) > maxTitleLength {
+			renderedTitle = renderedTitle[:maxTitleLength] + "..."
+		}
+
+		res := fmt.Sprintf("[%d/%d] %s", bar.Current(), len(res.List), renderedTitle)
+		for i := 0; i < maxTitleLength-len(currentTitle)+len("..."); i++ {
 			res += " "
 		}
 		return res
 	})
 
+	// Prepare the job queue
+	jobs := make(chan work, len(res.List))
+	results := make(chan work, len(res.List))
+
+	// Worker threads
+	for w := 0; w < parallelism; w++ {
+		go worker(jobs, results)
+	}
+
+	// Enqueue jobs.
 	for _, item := range res.List {
-		bar.Incr()
-		// We have to keep count of the max length of the title as we have to manually clear the line after \r.
-		title = item.Title()
-		if len(title) > maxTitleLength {
-			maxTitleLength = len(title)
+		jobs <- work{input: item}
+	}
+	close(jobs)
+
+	// Write results to file.
+	for range res.List {
+		res := <-results
+
+		if res.alreadyDownloaded {
+			fmt.Printf("Skipping %s\n\n", res.input.Title())
+			continue
 		}
 
-		fileName := outputDir + "/" + cleanFileName(title) + ".html"
-		if _, err := os.Stat(fileName); os.IsNotExist(err) || force {
-			article := Article{Item: item}
-			res, err := article.Download()
-			if err != nil {
-				fmt.Printf("Error downloading article: %s\n\n", err.Error())
-				continue
-			}
+		currentTitle = res.input.Title()
+		if res.err != nil {
+			fmt.Printf("Error downloading article: %s\n\n", res.err.Error())
+			continue
+		}
 
-			if err := ioutil.WriteFile(fileName, res, 0644); err != nil {
-				fmt.Printf("Error writing to file: %s\n\n", err.Error())
-				continue
-			}
+		if err := ioutil.WriteFile(res.outputFileName, res.output, 0644); err != nil {
+			fmt.Printf("Error writing to file: %s\n\n", err.Error())
+			continue
+		}
+		bar.Incr()
+	}
+
+}
+
+// work stores a downloadable article, and the readability result.
+type work struct {
+	input             api.Item
+	err               error
+	output            []byte
+	alreadyDownloaded bool
+	outputFileName    string
+}
+
+// worker handles downloading articles from a job queue.
+func worker(jobs <-chan work, results chan<- work) {
+	for j := range jobs {
+		title := j.input.Title()
+		outputFileName := outputDir + "/" + cleanFileName(title) + ".html"
+		if _, err := os.Stat(outputFileName); !os.IsNotExist(err) && !force {
+			results <- work{alreadyDownloaded: true, input: j.input}
+			continue
+		}
+
+		article := Article{Item: j.input}
+		res, err := article.Download()
+		results <- work{
+			input:          j.input,
+			output:         res,
+			err:            err,
+			outputFileName: outputFileName,
 		}
 	}
 }
