0385c19edd3baa4a0ffe76be5f836689c97cb5fa
Kornel Lugosi <coornail@gmail.com>
2018-06-02 12:41:35 +0200

    Add missing terminal.go

---
+++ terminal.go
@@ -0,0 +1,30 @@
+package main
+
+// Based on https://stackoverflow.com/a/16576712
+
+import (
+	"syscall"
+	"unsafe"
+)
+
+type winsize struct {
+	Row    uint16
+	Col    uint16
+	Xpixel uint16
+	Ypixel uint16
+}
+
+func getWidth() (uint, error) {
+	ws := &winsize{}
+	retCode, _, errno := syscall.Syscall(syscall.SYS_IOCTL,
+		uintptr(syscall.Stdin),
+		uintptr(syscall.TIOCGWINSZ),
+		uintptr(unsafe.Pointer(ws)))
+
+	var err error
+	if int(retCode) == -1 {
+		err = errno
+	}
+
+	return uint(ws.Col), err
+}
